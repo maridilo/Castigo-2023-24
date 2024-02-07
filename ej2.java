a3819fa246ca3391496529e1066616b245932672
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-07 19:31:05 +0100

    commit

---
+++ ej2.java
@@ -0,0 +1,10 @@
+public class ej2 {
+    public static void main(String[] args) {
+        //Muestra el mayor número que se puede representar con un char.
+        System.out.println("El mayor número que se puede representar con un char es " + (int) Character.MAX_VALUE + ".");
+        //Muestra el mayor número que se puede representar con un short.
+        System.out.println("El mayor número que se puede representar con un short es " + Short.MAX_VALUE + ".");
+        //Muestra el mayor número que se puede representar con un int.
+        System.out.println("El mayor número que se puede representar con un int es " + Integer.MAX_VALUE + ".");
+    }
+}
---
+++ ej3.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+public class ej3 {
+    public static void main(String[] args) {
+        int begin = 1;
+        int end = 100;
+
+        int[] multiplesOf5 = getMultiplesOf5(begin, end);
+        int sum = sumArray(multiplesOf5);
+
+        System.out.println("Multiples of 5:");
+        for (int multiple : multiplesOf5) {
+            System.out.println(multiple);
+        }
+
+        System.out.println("Sum of multiples of 5: " + sum);
+        System.out.println("Number of multiples of 5: " + multiplesOf5.length);
+    }
+
+    public static int[] getMultiplesOf5(int begin, int end) {
+        int count = 0;
+        for (int i = begin; i <= end; i++) {
+            if (i % 5 == 0) {
+                count++;
+            }
+        }
+
+        int[] multiples = new int[count];
+        int index = 0;
+        for (int i = begin; i <= end; i++) {
+            if (i % 5 == 0) {
+                multiples[index] = i;
+                index++;
+            }
+        }
+
+        return multiples;
+    }
+
+    public static int sumArray(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
+    }
+}
+
